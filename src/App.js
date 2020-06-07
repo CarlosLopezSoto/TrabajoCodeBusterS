0efca3e94ff9f21f4faf8b08012b3710e84b55ee
Mariano Troncoso <petotronco@gmail.com>
2020-06-07 20:36:28 -0300

    inicio nuevo front

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Code Busters</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Best Grupo 20202
         </p>
         <a
           className="App-link"
